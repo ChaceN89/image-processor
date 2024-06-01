38396cdc26253e1f237d9e59797948bc52dc9f89
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-06-01 14:11:20 -0600

    set up frontend railway file  and added host to allowed origins

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ CONVERTED_DIR.mkdir(parents=True, exist_ok=True)
 # set up middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Replace with your frontend's URL
+    allow_origins=[host],  # Replace with your frontend's URL
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
