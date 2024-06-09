8a85ab5585d972139b21ea39ca1ea9cd53478c22
Chace Nielson <60160140+ChaceN89@users.noreply.github.com>
2024-06-09 16:46:24 -0600

    fixe error in main.py for Cors policy

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ CONVERTED_DIR.mkdir(parents=True, exist_ok=True)
 # set up middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[""],  # Replace with your frontend's URL
+    allow_origins=["*"],  # Replace with your frontend's URL or * to allow all headers 
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
